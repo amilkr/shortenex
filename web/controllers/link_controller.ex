4466690fe834cd682a9b71625fc1056bd2063cbd
amilkr <amilcar.almonacid@gmail.com>
2015-02-11 10:01:20 -0300

    show page, redirecting to external url

--- web/controllers/link_controller.ex
+++ web/controllers/link_controller.ex
@@ -9,9 +9,21 @@ defmodule Shortenex.LinkController do
 
   def create(conn, %{"url" => url}) do
     link = Shortenex.Link.find_or_create(url: url)
-
+    shortened_url = "http://localhost:4000/s/#{link.code}"
     conn
-    |> put_flash(:notice, "Here you have the shortened url: http://localhost:4000/s/#{link.code}")
-    |> redirect to: "/"
+    |> assign(:shortened_url, shortened_url)
+    |> render :index
   end
+
+  def show(conn, %{"code" => code}) do
+    link = Shortenex.LinkQuery.find_by(:code, code) |> List.first
+    if link do
+      redirect(conn, external: link.url)
+    else
+      conn
+      |> put_flash(:error, "it seems your shortened url is not valid")
+      |> render :index
+    end
+  end
+  
 end
